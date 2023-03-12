ab827478770a6ece7aa4c6d6e36dce498c0469f9
Denis <stychynskidenis@gmail.com>
2023-03-12 16:29:00 +0300

    Добавили Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = SquareNumbers( number1, number2);
+
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool SquareNumbers(int num1,int num2)
+{
+    return num1 == num2 * num2 && num2 == num1 * num1;
+}
