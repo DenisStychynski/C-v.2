d2979d4fdc4a242dadece115b1921d954ed69f2c
Denis <stychynskidenis@gmail.com>
2023-03-12 17:15:54 +0300

    Убрал лишнее с Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -2,22 +2,6 @@
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int number3 = number1 % number2;
-if (number3 == 0)
-{
-    Console.WriteLine("Кратное");
-}
-else
-{
-    Console.WriteLine("Не кратное");
-}
-Console.Write(number3);
-/////////////////////////////////////////
-
-Console.WriteLine("Введите первое число");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int number2 = Convert.ToInt32(Console.ReadLine());
 
 int result = RemaiderDivision(number1,number2);
 
