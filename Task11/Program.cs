65caac5123da0253b9ecf97385be578ef5438b1b
Denis <stychynskidenis@gmail.com>
2023-03-12 16:28:48 +0300

    Добавили Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int RemoveSecondDigit (int num)
+{
+     int firstDigit = num / 100;
+     int thirDigit = num % 10;
+     int result = firstDigit * 10 + thirDigit;
+    
+     return result;
+}
+
+int number = new Random().Next(100, 1000);
+int removeSecondDigit = RemoveSecondDigit (number); 
+ Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+ Console.WriteLine($"Числа -> {removeSecondDigit} ");
