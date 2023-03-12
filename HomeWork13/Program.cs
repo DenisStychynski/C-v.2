9157b5c89378aa901006d6a87c6a73e279ea0f77
Denis <stychynskidenis@gmail.com>
2023-03-12 17:14:56 +0300

    Выполнил задачу 13

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,28 @@
+﻿int ShowSecondDigit (int num)
+{
+     
+     int result = -1;
+     if (num >=100)
+     {
+        while (num > 999)
+        {
+            num = num /10; 
+        }
+        result = num % 10;
+     } 
+    
+     return result;
+}
+
+int number = new Random().Next();
+int showSecondDigit = ShowSecondDigit (number); 
+if (number < 0 ) number = -number;
+Console.WriteLine($"Случайное число  -> {number}");
+
+if(ShowSecondDigit(number) == -1)
+Console.WriteLine ("Третьей цифры нет");
+
+else
+Console.WriteLine (showSecondDigit);
+
+
