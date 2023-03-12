ec4c1ae8f35192e950be669818b1cac7de573300
Denis <stychynskidenis@gmail.com>
2023-03-12 16:41:16 +0300

    Выполнили задачу 10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,12 @@
+﻿int ShowSecondDigit (int num)
+{
+     int secondDigit = num % 100;
+     int result = secondDigit / 10;
+    
+     return result;
+}
+
+int number = new Random().Next(100, 1000);
+int showSecondDigit = ShowSecondDigit (number); 
+ Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+ Console.WriteLine($"Числа -> {showSecondDigit} ");
