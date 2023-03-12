6d4d1e2e10eb3799747f3526fd9a119b0ec22fc1
Denis <stychynskidenis@gmail.com>
2023-03-12 17:31:14 +0300

    Сделали задачу 15

---
+++ HomeWork15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+    if (num1 >=1 && num1 <=5)
+        Console.WriteLine("Нет это не выходной");
+    else if (num1 >=6 && num1 <=7)
+        Console.WriteLine("Да, это выходной"); 
+    else 
+        Console.WriteLine("В неделе 7 дней, введи день от 1 до 7" );
