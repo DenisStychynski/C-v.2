d3e9bfbc4b121fa9bd60acb9ce9c57cc2c35c729
Denis <stychynskidenis@gmail.com>
2023-03-14 16:29:57 +0300

    Выполнили Task18

---
+++ Task 18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 18: Напишите программу, которая по
+//  заданному номеру четверти, показывает диапазон
+//  возможных координат точек в этой четверти (x и y).
+
+
+Console.WriteLine("Введите номер четверти");
+Console.WriteLine("number:");
+int  number= Convert.ToInt32(Console.ReadLine());
+
+
+string interval = Interval(number);
+
+Console.WriteLine ($"Указанная четверть соответствует координатам -> {interval}"); 
+               
+                
+
+string Interval(int quarter)
+{
+if(quarter == 1) return "x > 0, y > 0";
+if(quarter == 2) return "x < 0, y > 0";
+if(quarter == 3) return "x < 0, y < 0";
+if(quarter == 4) return "x > 0, y < 0";
+
+return "Такой четверти не существует";
+}
