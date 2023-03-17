b599c5abc7f9e6e4654a6e0ce9f9cba755933927
Denis <stychynskidenis@gmail.com>
2023-03-17 22:14:12 +0300

    Выполнили Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = new Random().Next(0, 2);
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+
+
+    }
+
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+
+}
