d158e523b4f03614387271b76643829e5a78ba9a
Denis <stychynskidenis@gmail.com>
2023-03-24 18:17:27 +0300

    Выполнили HomeWork34

---
+++ HomeWork34/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] array = CreateArrayRndInt(5, 100, 1000);
+PrintArray(array);
+Console.WriteLine();
+int evenNum = EvenNum(array);
+Console.WriteLine();
+Console.Write($"Количество чётных чисел в массиве: {evenNum}");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+int EvenNum(int[] array)
+{
+    int num = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            num++;
+    }
+    return num;
+}
