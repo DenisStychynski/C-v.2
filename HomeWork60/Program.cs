3b996117b986c5aefa0236e111a5bfe87af68bbb
Denis <stychynskidenis@gmail.com>
2023-04-05 11:40:41 +0300

    Выполнили HomeWork60

---
+++ HomeWork60/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+
+
+
+
+
+int[,,] array3d = new int[2, 2, 2];
+FillArray(array3d);
+PrintIndex(array3d);
+
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3d.GetLength(1); j++)
+        {
+            
+            
+            for (int k = 0; k < array3d.GetLength(2); k++)
+            {
+                Console.Write($"{array3d[i, j, k]}({i},{j},{k}) ");
+            }
+           
+            
+        }
+       Console.WriteLine();
+    }
+}
+
+
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
